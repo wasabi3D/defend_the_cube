c5117a631f18482c5e506cf6692d806f51c36faa
wasabi3D <57252663+wasabi3D@users.noreply.github.com>
2022-02-23 09:08:03 +0100

    Added new exception(Not a scene exeption)

--- GameManagement/Exceptions.py
+++ GameManagement/Exceptions.py
@@ -1,5 +1,8 @@
-
-
 class NoAvailableSceneException(Exception):
     def __init__(self):
         super().__init__("Couldn't find any game scene.")
+
+
+class NotASceneObjectException(Exception):
+    def __init__(self, message):
+        super().__init__(message)
