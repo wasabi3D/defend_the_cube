0d7954fe45bcd2db74d82a29c6e38b4189ca2be9
Louismarlane <louisphosy.martin@gmail.com>
2022-02-27 16:57:03 +0100

    implementation of biomes in the terrain generation with Voronoi's noise

--- generate_terrain.py
+++ generate_terrain.py
@@ -6,8 +6,8 @@ import pygame
 
 pygame.init()
 
-GRID_SIZE = (600, 400)
-SIZE_BLOCK = 2
+GRID_SIZE = (200, 200)
+SIZE_BLOCK = 4
 SCREEN_SIZE = (GRID_SIZE[0] * SIZE_BLOCK, GRID_SIZE[1] * SIZE_BLOCK)
 
 
@@ -18,26 +18,31 @@ class Terrain:
 
     def __init__(self, seed: int,
                  size: tuple[int, int],
+                 biome_types: list[pygame.Surface],
                  scale: float = 0.03,
                  forest_size_scale: float = 0.08,
-                 forest_desity_scale: float = 2.,
+                 forest_density_scale: float = 2.,
                  beach_width: int = 3,
                  water_limit: float = -0.5,
                  tree_lim: float = 0.3,
                  tree_dens_lim: float = 0.3,
-                 biome_chunk_size: int = 20
+                 biome_chunk_size: int = 20,
+                 minkowski_exponent: float = 2.
                  ) -> None:
         """
         :param seed: seed: graine du monde
         :param size: size: taille de la carte
+        :param biome_types: types de biomes (image du carré)
         :param scale: scale: Échelle de bruit Simplex pour la génération de l'eau et l'herbe
         :param forest_size_scale: forest_size_scale: Échelle de bruit Simplex pour les zones de forêt
-        :param forest_desity_scale: forest_desity_scale: Échelle de bruit Simplex pour la densité des forêts
+        :param forest_density_scale: forest_desity_scale: Échelle de bruit Simplex pour la densité des forêts
         :param beach_width: Modifie jusquà quelle distance vont les plages
         :param water_limit: Modifie la taille dea plages mais pas la fréquance
         :param tree_lim: modifie la taille des forêts sans leur fréquance
         :param tree_dens_lim: Modifie la taille des groupes d'arbres
                dans les forêts (si forest density scale est assez basse)
+        :param biome_chunk_size: Taille des tronçons (modifie la taille des biomes
+        :param minkowski_exponent: exponentiel pour calculer les distances de Minkowski pour le bruit de Veronoi
         """
 
         self.seed = seed
@@ -48,7 +53,7 @@ class Terrain:
 
         # Variables en relation avec la forêt
         self.forest_size_scale = forest_size_scale
-        self.forest_density_scale = forest_desity_scale
+        self.forest_density_scale = forest_density_scale
 
         self.beach_width = beach_width
 
@@ -56,13 +61,15 @@ class Terrain:
         self.tree_lim = tree_lim
         self.tree_dens_lim = tree_dens_lim
 
-        self.biome_chunk_size = biome_chunk_size
-
         d = os.getcwd() + r"\resources\test\grid"
         self.GRASS = pygame.transform.scale(pygame.image.load(d + r"\grid_two.png"), (SIZE_BLOCK, SIZE_BLOCK))
         self.SAND = pygame.transform.scale(pygame.image.load(d + r"\grid_one.png"), (SIZE_BLOCK, SIZE_BLOCK))
         self.WATER = pygame.transform.scale(pygame.image.load(d + r"\grid_three.png"), (SIZE_BLOCK, SIZE_BLOCK))
         self.TREE = pygame.transform.scale(pygame.image.load(d + r"\tree.png"), (SIZE_BLOCK, SIZE_BLOCK))
+
+        self.biome_types = biome_types
+        self.voronoi = Voronoi(seed=seed + 11, chunk_size=biome_chunk_size,
+                               chunk_types=biome_types, minkowski_exponent=minkowski_exponent)
 
         self.create_terrain(self.per, size)
 
@@ -86,7 +93,7 @@ class Terrain:
                 if tmp < self.water_limit:
                     self.terrain[y].append(self.WATER)
                 else:
-                    self.terrain[y].append(self.GRASS)
+                    self.terrain[y].append(self.voronoi.noise2(x, y))
                 self.over_terrain[y].append(None)
 
         # ici on rajouter les blocks de sable à la place des blocks d'herbe
@@ -97,12 +104,12 @@ class Terrain:
                     for x2 in range(x - self.beach_width, x + self.beach_width + 1):
                         for y2 in range(y - self.beach_width, y + self.beach_width + 1):
                             if 0 <= x2 < size[0] and 0 <= y2 < size[1]:
-                                random.seed(x2 + y2)
-                                if self.terrain[y2][x2] == self.GRASS and \
+                                random.seed(x2 * y2)
+                                # on enlève le sable qui n'est ni à côté de l'eau ou de plus de sable
+                                if self.terrain[y2][x2] in self.biome_types and \
                                         self.get_distance_squared((x, y), (x2, y2)) \
                                         <= random.uniform(0, self.beach_width) ** 2:
                                     self.terrain[y2][x2] = self.SAND
-        # on enlève le sable qui n'est ni à côté de l'eau ou de plus de sable
         # on en profite pour générer des arbres sur une liste différente au terrain (mais en s'y ajustant)
         tree_zones_noise = op.OpenSimplex(self.seed + 100)
         for y, line in enumerate(self.terrain):
@@ -119,9 +126,9 @@ class Terrain:
                                 tmp_change = False
                                 break
                     if tmp_change:
-                        self.terrain[y][x] = self.GRASS
+                        self.terrain[y][x] = self.voronoi.noise2(x, y)
                 # arbres
-                if self.terrain[y][x] == self.GRASS:
+                if self.terrain[y][x] in self.biome_types:
                     if tree_zones_noise.noise2(
                             x * self.forest_size_scale, y * self.forest_size_scale
                     ) > self.tree_lim and tree_zones_noise.noise2(
@@ -130,10 +137,17 @@ class Terrain:
                         self.over_terrain[y][x] = self.TREE
 
 
+# Classe permettant de générer un bruit de Voronoi, bruit permettant de délimiter des zones dans notre cas
 class Voronoi:
     points = {}
 
     def __init__(self, seed: int, chunk_size: int, chunk_types: list, minkowski_exponent: float = 2):
+        """
+        :param seed: graine de la génération
+        :param chunk_size: taille des tronçons (répartition des points
+        :param chunk_types: différants types de points (zones ou biomes)
+        :param minkowski_exponent: l'exponentiel pour le calcul des distances (2 == théorème de Pythagore)
+        """
         assert chunk_size > 0, "Les tronçons doivent éxister! (leur taille doit être superieure à 0)"
         assert len(chunk_types) > 0, "Il doit y avoir au mois un type de biome!"
 
@@ -141,16 +155,22 @@ class Voronoi:
 
         self.chunk_size = chunk_size
         self.chunk_types = chunk_types
-        n_biomes = len(chunk_types)
-        self.chunk_type_limits = [2 * (a + 1) / n_biomes - 1 for a in range(n_biomes - 1)]
 
         self.minkowski_exponent = minkowski_exponent
 
-        self.rand_pos_x = op.OpenSimplex(seed)
-        self.rand_pos_y = op.OpenSimplex(seed + 10)
-
     def noise2(self, x, y):
+        """ Cette fonction nous permet d'avoir le type du point de coordonnées données
+        qui est choisi au hazar dans les types de tronçons
+        :param x: position x du point
+        :param y: position y du point
+        :return: type du point (donné dans la création de l'objet)
+        """
         def minkowski_distance_exp(x2: int, y2: int):
+            """ Calcule la distance avec la formule de Minkowski à la puissance
+            :param x2: position x du point
+            :param y2: position y du point
+            :return: la distance de Minkowski entre le point donné et le point de coordonées (x, y)
+            """
             return abs(x - x2) ** self.minkowski_exponent + abs(y - y2) ** self.minkowski_exponent
         # On repère les coordonées du tronçon dans lequel le point se situe
         chunk_x = x // self.chunk_size
@@ -162,7 +182,9 @@ class Voronoi:
             points.append([])
             for i in range(chunk_x - 2, chunk_x + 3):
                 c_pos = (i, j)
+                # On utilise un diccionnaire pour accélérer la génération
                 if c_pos not in self.points:
+                    # les seed servent à avoir toujours la même chose même si on les répète plusieurs fois
                     random.seed(c_pos[1] * c_pos[0] + self.seed + 20)
                     self.points[c_pos] = (
                         random.randint(0, self.chunk_size - 1),
@@ -170,6 +192,7 @@ class Voronoi:
                     )
                 points[-1].append(self.points[c_pos])
 
+        # à partir d'ici on cherche le tronçaon dont le point est le plus proche
         closest_point_chunk = (chunk_x - 2, chunk_y - 2)
         dist = minkowski_distance_exp(self.chunk_size * (chunk_x - 2) + points[0][0][0],
                                       self.chunk_size * (chunk_y - 2) + points[0][0][1])
@@ -182,23 +205,24 @@ class Voronoi:
                     closest_point_chunk = (true_ix, true_iy)
                     dist = tmp_dist
 
-        random.seed(sum(closest_point_chunk) + self.seed)
+        # finalement on utilise les seed pour que tous les points dans la zone du tronçon soient du même type
+        random.seed(closest_point_chunk[0] * closest_point_chunk[1] + self.seed)
         return random.choice(self.chunk_types)
 
 
-_d = os.getcwd() + r"\resources\test\grid"
-_GRASS = pygame.transform.scale(pygame.image.load(_d + r"\grid_two.png"), (SIZE_BLOCK, SIZE_BLOCK))
-_SAND = pygame.transform.scale(pygame.image.load(_d + r"\grid_one.png"), (SIZE_BLOCK, SIZE_BLOCK))
-_WATER = pygame.transform.scale(pygame.image.load(_d + r"\grid_three.png"), (SIZE_BLOCK, SIZE_BLOCK))
-_LAVA = pygame.transform.scale(pygame.image.load(_d + r"\grid_four.png"), (SIZE_BLOCK, SIZE_BLOCK))
-
 # Test de la génération
 if __name__ == "__main__":
+    _d = os.getcwd() + r"\resources\test\grid"
+    _GRASS = pygame.transform.scale(pygame.image.load(_d + r"\grid_two.png"), (SIZE_BLOCK, SIZE_BLOCK))
+    _SAND = pygame.transform.scale(pygame.image.load(_d + r"\grid_one.png"), (SIZE_BLOCK, SIZE_BLOCK))
+    _WATER = pygame.transform.scale(pygame.image.load(_d + r"\grid_three.png"), (SIZE_BLOCK, SIZE_BLOCK))
+    _LAVA = pygame.transform.scale(pygame.image.load(_d + r"\grid_four.png"), (SIZE_BLOCK, SIZE_BLOCK))
+    _DARK_GRASS = pygame.transform.scale(pygame.image.load(_d + r"\dark_grass.png"), (SIZE_BLOCK, SIZE_BLOCK))
+
+    ter = Terrain(202, size=GRID_SIZE, minkowski_exponent=4, biome_types=[_GRASS, _DARK_GRASS])
+
     screen = pygame.display.set_mode(SCREEN_SIZE)
 
-    ter = Terrain(202, GRID_SIZE)
-    v = Voronoi(3, 20, [_GRASS, _SAND, _WATER, _LAVA], minkowski_exponent=4)
-    # tab = [[v.noise2(j, i) for j in range(GRID_SIZE[0])]for i in range(GRID_SIZE[1])]
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -208,13 +232,10 @@ if __name__ == "__main__":
         for i, t in enumerate(ter.terrain):
             for j, c in enumerate(t):
                 screen.blit(c, c.get_rect(topleft=(j * SIZE_BLOCK, i * SIZE_BLOCK)))
-                if ter.over_terrain[i][j] == ter.TREE:
+                if ter.over_terrain[i][j] is not None:
                     screen.blit(
                         ter.over_terrain[i][j],
                         ter.over_terrain[i][j].get_rect(topleft=(j * SIZE_BLOCK, i * SIZE_BLOCK))
                     )
-        # for i, t in enumerate(tab):
-        #     for j, c in enumerate(t):
-        #         screen.blit(c, c.get_rect(topleft=(j * SIZE_BLOCK, i * SIZE_BLOCK)))
 
         pygame.display.update()
